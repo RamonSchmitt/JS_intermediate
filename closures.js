634dfe855e3613db30806039a137a6492aa3c414
RamonSchmitt <ramon.schmitt@yahoo.com>
2016-10-20 11:48:30 +0200

    private storage

--- closures.js
+++ closures.js
@@ -1,4 +1,4 @@
-// What is closure?
+// What is Closure?
 var publicFunction = function() {
   var privateVariable = "something";
   var innerFunction = function() {
@@ -9,3 +9,35 @@ var publicFunction = function() {
 
 var privateFunction = publicFunction();
 console.log(privateFunction());
+
+// Uses for Closures
+var tracking = {};
+function trackActions(item, choice) {
+  if (choice) {
+    tracking[item] = choice;
+  }
+  return tracking[item];
+}
+trackActions("red", "click");
+console.log(trackActions("red"));
+console.log(tracking.red);
+
+function actionTracker(choice) {
+  var tracking = {};
+  return function(action) {
+    if (action) {
+      tracking[choice] = action;
+    }
+    return tracking[choice];
+  };
+}
+
+var redTracker = actionTracker("red");
+var blueTracker = actionTracker("blue");
+var secretTracker = actionTracker("secret");
+
+console.log(blueTracker());
+blueTracker("click");
+console.log(blueTracker());
+redTracker("touch");
+console.log(redTracker());
