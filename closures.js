58752d2b751224db6f907d9c36640d0848212eb9
RamonSchmitt <ramon.schmitt@yahoo.com>
2016-10-21 15:31:53 +0200

    protecting

--- closures.js
+++ closures.js
@@ -59,3 +59,55 @@ function actionTracker(choice) {
 var redTracker = actionTracker("red");
 redTracker.setAction("click");
 console.log(redTracker.getAction());
+
+// Surprising Closure behaviour
+var counter;
+var functions = [];
+var values = [];
+for (counter = 0; counter < 5; counter++) {
+  values[counter] = counter;
+  functions[counter] = function() {
+    return counter;
+  };
+}
+console.log(values[0]);
+console.log(values[2]);
+console.log(values[4]);
+console.log(functions[0]());
+console.log(functions[2]());
+console.log(functions[4]());
+
+var counter;
+var functions = [];
+var values = [];
+var makeReturner = function(value) {
+  return function() {
+    return value;
+  };
+};
+for(counter = 0; counter < 5; counter++) {
+  values[counter] = counter;
+  functions[counter] = makeReturner(counter);
+}
+console.log(values[0]);
+console.log(values[2]);
+console.log(values[4]);
+console.log(functions[0]());
+console.log(functions[2]());
+console.log(functions[4]());
+
+// ES6
+var functions = [];
+var values = [];
+for (let counter = 0; counter < 5; counter++) {
+  values[counter] = counter;
+  functions[counter] = function() {
+    return counter;
+  };
+}
+console.log(values[0]);
+console.log(values[2]);
+console.log(values[4]);
+console.log(functions[0]());
+console.log(functions[2]());
+console.log(functions[4]());
